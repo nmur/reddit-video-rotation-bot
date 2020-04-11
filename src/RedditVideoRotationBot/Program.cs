9a02f2211a57ea724ba3f35c6d149ff605ee7678
Nicholas Murray <nickmurray91@gmail.com>
2020-04-12 02:52:33 +1000

    Revert "changed project to web proj so it can respond to container health checks"

--- src/RedditVideoRotationBot/Program.cs
+++ src/RedditVideoRotationBot/Program.cs
@@ -1,15 +1,11 @@
-using System;
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Hosting;
+﻿using System;
 
 namespace RedditVideoRotationBot
 {
-    public class Program
+    class Program
     {
-        public static void Main(string[] args)
+        static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
-
             var counter = 0;
             var max = args.Length != 0 ? Convert.ToInt32(args[0]) : -1;
             while (max == -1 || counter < max)
@@ -18,18 +14,6 @@ namespace RedditVideoRotationBot
                 Console.WriteLine($"Counter: {counter}");
                 System.Threading.Tasks.Task.Delay(10000).Wait();
             }
-        }
-
-        public static IHostBuilder CreateHostBuilder(string[] args)
-        {
-            string port = Environment.GetEnvironmentVariable("PORT") ?? "8080";
-            string url = string.Concat("http://0.0.0.0:", port);
-
-            return Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder =>
-                {
-                webBuilder.UseStartup<Startup>();
-                });
         }
     }
 }
