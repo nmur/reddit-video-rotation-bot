7535d5d96f0f00dbfe5d583a36350cec35830c82
Nicholas Murray <nickmurray91@gmail.com>
2020-04-14 16:01:20 +1000

    added circleci config and a failing test

--- tst/unit/RedditVideoRotationBotTests/UnitTest1.cs
+++ tst/unit/RedditVideoRotationBotTests/UnitTest1.cs
@@ -15,7 +15,7 @@ namespace RedditVideoRotationBotTests
             var result = worker.IncrementInt(1);
 
             // Assert
-            Assert.Equal(2, result);
+            Assert.Equal(3, result);
         }
     }
 }
