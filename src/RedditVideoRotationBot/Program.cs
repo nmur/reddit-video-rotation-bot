aeb97d99d9f313dad53b8d53cf532e4a421a8c81
Nicholas Murray <nickmurray91@gmail.com>
2020-04-16 14:28:39 +1000

    intermediate commit to store experimentation

--- src/RedditVideoRotationBot/Program.cs
+++ src/RedditVideoRotationBot/Program.cs
@@ -1,18 +1,11 @@
-﻿using System;
-
-namespace RedditVideoRotationBot
+﻿namespace RedditVideoRotationBot
 {
     public class Program
     {
         static void Main(string[] args)
         {
-            var counter = 0;
-            while (counter < int.MaxValue)
-            {
-                counter++;
-                Console.WriteLine($"Counter: {counter}!!!");
-                System.Threading.Tasks.Task.Delay(5000).Wait();
-            }
+            var worker = new RedditHelper();
+            worker.ReadMessages();
         }
     }
 }
--- tst/unit/RedditVideoRotationBotTests/UnitTest1.cs
+++ tst/unit/RedditVideoRotationBotTests/UnitTest1.cs
@@ -8,14 +8,6 @@ namespace RedditVideoRotationBotTests
         [Fact]
         public void Test1()
         {
-            // Arrange
-            var worker = new Worker();
-
-            // Act
-            var result = worker.IncrementInt(1);
-
-            // Assert
-            Assert.Equal(2, result);
         }
     }
 }
