f2ef1c261a902ec7cfd500d6c5c0c5de2ee73a36
Nicholas Murray <nickmurray91@gmail.com>
2020-04-17 23:57:23 +1000

    created test for unread monitoring from previous branch

--- tst/unit/RedditVideoRotationBotTests/RedditHelperTests.cs
+++ tst/unit/RedditVideoRotationBotTests/RedditHelperTests.cs
@@ -12,5 +12,19 @@ namespace RedditVideoRotationBotTests
             var fakeRedditClientWrapper = A.Fake<IRedditClientWrapper>();
             new RedditHelper(fakeRedditClientWrapper);
         }
+
+        [Fact]
+        public void GivenRedditHelper_WhenMonitorUnreadMessagesIsCalled_ThenUnreadMessagesAreMonitored()
+        {
+            // Arrange
+            var fakeRedditClientWrapper = A.Fake<IRedditClientWrapper>();
+            var redditHelper = new RedditHelper(fakeRedditClientWrapper);
+
+            // Act
+            redditHelper.MonitorUnreadMessages();
+
+            // Assert
+            A.CallTo(() => fakeRedditClientWrapper.MonitorUnread()).MustHaveHappenedOnceExactly();
+        }
     }
 }
