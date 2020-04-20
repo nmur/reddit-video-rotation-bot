67ea32522576bf0631ed8f185b038322385b8d42
Nicholas Murray <nickmurray91@gmail.com>
2020-04-20 15:41:18 +1000

    created a little parser class for Reddit posts

---
+++ src/RedditVideoRotationBot/RedditPostParser.cs
@@ -0,0 +1,28 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using Reddit.Things;
+
+namespace RedditVideoRotationBot
+{
+    public static class RedditPostParser
+    {
+        public static string TryGetVideoUrlFromPost(Post post)
+        {
+            var mediaJObject = (JObject)post.Media;
+            var media = mediaJObject.ToObject<Media>();
+            return media.RedditVideo.FallbackUrl;
+        }
+    }
+
+    internal class Media
+    {
+        [JsonProperty("reddit_video")]
+        public RedditVideo RedditVideo;
+    }
+
+    internal class RedditVideo
+    {
+        [JsonProperty("fallback_url")]
+        public string FallbackUrl;
+    }
+}
