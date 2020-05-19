513065f344cde3f4863f2a8c91c22569aa7e8a57
Nicholas Murray <nickmurray91@gmail.com>
2020-05-19 17:10:08 +1000

    created the bare minimal test for the new video uploader class

---
+++ tst/unit/RedditVideoRotationBotTests/GfyCatVideoUploaderTests.cs
@@ -0,0 +1,60 @@
+﻿using FakeItEasy;
+using RedditVideoRotationBot;
+using RedditVideoRotationBot.Interfaces;
+using Refit;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace RedditVideoRotationBotTests
+{
+    public class GfyCatVideoUploaderTests
+    {
+        private readonly IGfyCatApi _fakeGfyCatApi;
+
+        private readonly IGfyCatFileDropApi _fakeGfyCatFileDropApi;
+
+        private readonly IVideoUploader _gfyCatVideoUploader;
+
+        public GfyCatVideoUploaderTests()
+        {
+            _fakeGfyCatApi = A.Fake<IGfyCatApi>();
+            _fakeGfyCatFileDropApi = A.Fake<IGfyCatFileDropApi>();
+            _gfyCatVideoUploader = new GfyCatVideoUploader(_fakeGfyCatApi, _fakeGfyCatFileDropApi);
+        }
+
+        [Fact]
+        public async Task GivenRotatedVideoExists_WhenVideoUploadIsCalled_ThenVideoFileIsUploaded()
+        {
+            // Arrange
+            CreateRotatedVideoFile();
+
+            A.CallTo(() => _fakeGfyCatApi.GetAuthToken(A<GfyCatCredentials>._))
+                .Returns(new GfyCatTokenResponse
+                {
+                    AccessToken = "fake_token"
+                });
+
+            A.CallTo(() => _fakeGfyCatApi.CreateGfy(A<string>._))
+                .Returns(new GfyCreationResponse
+                {
+                    IsOk = true,
+                    GfyName = "fake_gfyName"
+                });
+
+            // Act
+            await _gfyCatVideoUploader.UploadAsync();
+
+            // Assert
+            A.CallTo(() => _fakeGfyCatFileDropApi.UploadVideoFromFile(A<string>._, A<StreamPart>._)).MustHaveHappenedOnceExactly();
+        }
+
+        private static void CreateRotatedVideoFile()
+        {
+            using var fs = File.Create("video_rotated.mp4");
+            var info = new UTF8Encoding(true).GetBytes("Adding some text into the file.");
+            fs.Write(info, 0, info.Length);
+        }
+    }
+}
