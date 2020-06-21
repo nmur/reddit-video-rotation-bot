21f6d79626187d6f0748b538a2c5a16d7e1b1843
Nicholas Murray <nickmurray91@gmail.com>
2020-06-22 00:20:46 +1000

    renamed video rotator implementation to describe the rotation technology used

--- src/RedditVideoRotationBot/FfmpegVideoRotator.cs
+++ src/RedditVideoRotationBot/FfmpegVideoRotator.cs
@@ -8,18 +8,20 @@ using System.IO;
 namespace RedditVideoRotationBot
 {
     [ExcludeFromCodeCoverage] //TODO: should be integration tested though
-    public class VideoRotator : IVideoRotator
+    public class FfmpegVideoRotator : IVideoRotator
     {
         public void Rotate(string rotationArgument)
         {
             if (File.Exists("video.mp4"))
             {
+                var rotationValue = GetRotationValue();
+
                 var proc = new Process
                 {
                     StartInfo = new ProcessStartInfo
                     {
                         FileName = "ffmpeg",
-                        Arguments = "-i video.mp4 -c copy -metadata:s:v:0 rotate=90 video_rotated.mp4",
+                        Arguments = $"-i video.mp4 -c copy -metadata:s:v:0 rotate={rotationValue} video_rotated.mp4",
                         UseShellExecute = false,
                         RedirectStandardOutput = true,
                         RedirectStandardError = true
@@ -38,6 +40,11 @@ namespace RedditVideoRotationBot
             {
                 throw new VideoRotateException("Video rotation failed.");
             }
+        }
+
+        private static string GetRotationValue()
+        {
+            return "90";
         }
     }
 }
