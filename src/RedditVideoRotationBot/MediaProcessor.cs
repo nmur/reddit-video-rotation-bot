1bded67327f1024a258dd145940e212f156537aa
Nicholas Murray <nickmurray91@gmail.com>
2020-07-05 20:46:55 +1000

    implemented video/audio combination functionality

---
+++ src/RedditVideoRotationBot/MediaProcessor.cs
@@ -0,0 +1,31 @@
+﻿using System.Diagnostics;
+using System.Diagnostics.CodeAnalysis;
+using System.IO;
+
+namespace RedditVideoRotationBot
+{
+    [ExcludeFromCodeCoverage]
+    public class MediaProcessor : IMediaProcessor
+    {
+        public void CombineVideoAndAudio()
+        {
+            if (File.Exists("video.mp4") && File.Exists("audio.mp4"))
+            {
+                var proc = new Process
+                {
+                    StartInfo = new ProcessStartInfo
+                    {
+                        FileName = "ffmpeg",
+                        Arguments = "-i video.mp4 -i audio.mp4 -c copy video.mp4",
+                        UseShellExecute = false,
+                        RedirectStandardOutput = true,
+                        RedirectStandardError = true
+                    }
+                };
+
+                proc.Start();
+                proc.WaitForExit();
+            }
+        }
+    }
+}
