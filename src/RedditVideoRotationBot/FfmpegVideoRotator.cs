c64e239e7f513a5244ad216743c01f9f53e113f7
Nicholas Murray <nickmurray91@gmail.com>
2020-07-05 20:56:32 +1000

    created a new smaller class for purely executing ffmpeg commands, for added test coverage

--- src/RedditVideoRotationBot/FfmpegVideoRotator.cs
+++ src/RedditVideoRotationBot/FfmpegVideoRotator.cs
@@ -1,40 +1,30 @@
 ﻿using RedditVideoRotationBot.Exceptions;
 using RedditVideoRotationBot.Interfaces;
 using System;
-using System.Diagnostics;
-using System.Diagnostics.CodeAnalysis;
 using System.IO;
 
 namespace RedditVideoRotationBot
 {
-    [ExcludeFromCodeCoverage] //TODO: should be integration tested though
     public class FfmpegVideoRotator : IVideoRotator
     {
+        private readonly IFfmpegExecutor _ffmpegExecutor;
+
+        public FfmpegVideoRotator(IFfmpegExecutor ffmpegExecutor)
+        {
+            _ffmpegExecutor = ffmpegExecutor;
+        }
+
         public void Rotate(string messageArg)
         {
             if (File.Exists("video.mp4"))
             {
                 var rotationArg = FfmpegRotationArgumentDeterminer.GetRotationArgFromMessageArg(messageArg);
-
-                var proc = new Process
-                {
-                    StartInfo = new ProcessStartInfo
-                    {
-                        FileName = "ffmpeg",
-                        Arguments = $"-i video.mp4 -c copy -metadata:s:v:0 rotate={rotationArg} video_rotated.mp4",
-                        UseShellExecute = false,
-                        RedirectStandardOutput = true,
-                        RedirectStandardError = true
-                    }
-                };
-
-                proc.Start();
-                proc.WaitForExit();
+                _ffmpegExecutor.ExecuteFfmpegCommandWithArgString($"-i video.mp4 -c copy -metadata:s:v:0 rotate={rotationArg} video_rotated.mp4");
             }
 
             if (File.Exists("video_rotated.mp4"))
             {
-                Console.WriteLine($"video rotated successfully!");
+                Console.WriteLine("video rotated successfully!");
             }
             else
             {
--- src/RedditVideoRotationBot/MediaProcessor.cs
+++ src/RedditVideoRotationBot/MediaProcessor.cs
@@ -1,30 +1,22 @@
-﻿using System.Diagnostics;
-using System.Diagnostics.CodeAnalysis;
+﻿using RedditVideoRotationBot.Interfaces;
 using System.IO;
 
 namespace RedditVideoRotationBot
 {
-    [ExcludeFromCodeCoverage]
     public class MediaProcessor : IMediaProcessor
     {
+        private readonly IFfmpegExecutor _ffmpegExecutor;
+
+        public MediaProcessor(IFfmpegExecutor ffmpegExecutor)
+        {
+            _ffmpegExecutor = ffmpegExecutor;
+        }
+
         public void CombineVideoAndAudio()
         {
             if (File.Exists("video.mp4") && File.Exists("audio.mp4"))
             {
-                var proc = new Process
-                {
-                    StartInfo = new ProcessStartInfo
-                    {
-                        FileName = "ffmpeg",
-                        Arguments = "-i video.mp4 -i audio.mp4 -c copy video.mp4",
-                        UseShellExecute = false,
-                        RedirectStandardOutput = true,
-                        RedirectStandardError = true
-                    }
-                };
-
-                proc.Start();
-                proc.WaitForExit();
+                _ffmpegExecutor.ExecuteFfmpegCommandWithArgString("-i video.mp4 -i audio.mp4 -c copy video.mp4");
             }
         }
     }
