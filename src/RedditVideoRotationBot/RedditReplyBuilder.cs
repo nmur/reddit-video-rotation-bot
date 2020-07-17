9e6e7e698873c493ac4fb72b687b8993dbd32a40
Nicholas Murray <nickmurray91@gmail.com>
2020-07-18 01:58:44 +1000

    refactored

--- src/RedditVideoRotationBot/RedditReplyBuilder.cs
+++ src/RedditVideoRotationBot/RedditReplyBuilder.cs
@@ -4,9 +4,17 @@ namespace RedditVideoRotationBot
 {
     public class RedditReplyBuilder : IReplyBuilder
     {
+        private readonly string ReplyTemplate = $"Rotated Video: {{0}}  \r\n\r\n***\r\n^^[usage]({UsageUrl})&nbsp;-&nbsp;[source]({SourceUrl})&nbsp;-&nbsp;[pm&nbsp;me]({PmUrl})";
+
+        private const string UsageUrl = "https://github.com/nmur/reddit-video-rotation-bot/wiki/Detailed-usage-instructions";
+
+        private const string SourceUrl = "https://github.com/nmur/reddit-video-rotation-bot";
+
+        private const string PmUrl = "https://www.reddit.com/message/compose/?to=nmur";
+
         public string BuildReply(string uploadedVideoUrl)
         {
-            return uploadedVideoUrl;
+            return string.Format(ReplyTemplate, uploadedVideoUrl);
         }
     }
 }
--- tst/unit/RedditVideoRotationBotTests/RedditReplyBuilderTests.cs
+++ tst/unit/RedditVideoRotationBotTests/RedditReplyBuilderTests.cs
@@ -5,20 +5,27 @@ namespace RedditVideoRotationBotTests
 {
     public class RedditReplyBuilderTests
     {
+        private readonly string CompletedReply = $"Rotated Video: {UploadedVideoUrl}  \r\n\r\n***\r\n^^[usage]({UsageUrl})&nbsp;-&nbsp;[source]({SourceUrl})&nbsp;-&nbsp;[pm&nbsp;me]({PmUrl})";
+
         private const string UploadedVideoUrl = "https://giant.gfycat.com/SomeFakeVideo.mp4";
+
+        private const string UsageUrl = "https://github.com/nmur/reddit-video-rotation-bot/wiki/Detailed-usage-instructions";
+
+        private const string SourceUrl = "https://github.com/nmur/reddit-video-rotation-bot";
+
+        private const string PmUrl = "https://www.reddit.com/message/compose/?to=nmur";
 
         [Fact]
         public void GivenUploadedVideoUrl_WhenRedditReplyIsBuilt_ThenRedditReplyIsReturnedSuccessfully()
         {
             // Arrange
             var redditReplyBuilder = new RedditReplyBuilder();
-            var expectedReply = $"Rotated Video: {UploadedVideoUrl}  \r\n\r\n***\r\n^^[usage](https://github.com/nmur/reddit-video-rotation-bot/wiki/Detailed-usage-instructions)&nbsp;-&nbsp;[source](https://github.com/nmur/reddit-video-rotation-bot)&nbsp;-&nbsp;[pm&nbsp;me](https://www.reddit.com/message/compose/?to=nmur)";
 
             // Act
             var reply = redditReplyBuilder.BuildReply(UploadedVideoUrl);
 
             // Assert
-            Assert.Equal(expectedReply, reply);
+            Assert.Equal(CompletedReply, reply);
         }
     }
 }
